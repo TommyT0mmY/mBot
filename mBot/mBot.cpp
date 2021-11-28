20419786fe421d8c782a3e7f2767bad9a6baa06a
sortinodavide9 <sortinodavide9@gmail.com>
2021-11-28 14:38:50 +0100

    Apertura cam

--- mBot/mBot.cpp
+++ mBot/mBot.cpp
@@ -1,20 +1,30 @@
-// mBot.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
-//
-
+#include <opencv2\cvconfig.h>
+#include <opencv2\highgui\highgui.hpp>
 #include <iostream>
+
+#define CAM_NUMBER 2
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	//apertura camera
+	cv::VideoCapture cap(CAM_NUMBER);
+	if (!cap.isOpened()) {
+		std::cout << "Opening cam Error! change CAM_NUMBER";
+		return -1;
+	}
+
+	//frame showing
+	while (1) {
+		cv::Mat frame;
+		cap.read(frame);
+		imshow("camera", frame);
+		if (cv::waitKey(30) == 27) {
+			cap.release();//clears the camera buffer
+			return 0; //esc
+		}
+	}
+
+
 }
 
-// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
-// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
-
-// Suggerimenti per iniziare: 
-//   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
-//   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
-//   3. Usare la finestra di output per visualizzare l'output di compilazione e altri messaggi
-//   4. Usare la finestra Elenco errori per visualizzare gli errori
-//   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
-//   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln

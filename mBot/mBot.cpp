d0b7ceb6ae92ca19dfa0fba319950df0b4bddb61
EMALOK <emanuele.riccilucchi@gmail.com>
2021-11-28 19:25:58 +0100

    git reset opencv in c:\opencv senza utilizzare nuget

--- mBot/mBot.cpp
+++ mBot/mBot.cpp
@@ -1,30 +1,82 @@
-#include <opencv2\cvconfig.h>
-#include <opencv2\highgui\highgui.hpp>
+#pragma once
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 
-#define CAM_NUMBER 2
+using namespace cv;
+using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
+    VideoCapture cap(0); //capture the video from web cam
 
-	//apertura camera
-	cv::VideoCapture cap(CAM_NUMBER);
-	if (!cap.isOpened()) {
-		std::cout << "Opening cam Error! change CAM_NUMBER";
-		return -1;
-	}
+    if (!cap.isOpened())  // if not success, exit program
+    {
+        cout << "Cannot open the web cam" << endl;
+        return -1;
+    }
 
-	//frame showing
-	while (1) {
-		cv::Mat frame;
-		cap.read(frame);
-		imshow("camera", frame);
-		if (cv::waitKey(30) == 27) {
-			cap.release();//clears the camera buffer
-			return 0; //esc
-		}
-	}
+    namedWindow("Control"); //create a window called "Control"
 
+    int iLowH = 0;
+    int iHighH = 179;
+
+    int iLowS = 0;
+    int iHighS = 255;
+
+    int iLowV = 0;
+    int iHighV = 255;
+
+    //Create trackbars in "Control" window
+    createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    createTrackbar("HighH", "Control", &iHighH, 179);
+
+    createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    createTrackbar("HighS", "Control", &iHighS, 255);
+
+    createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    createTrackbar("HighV", "Control", &iHighV, 255);
+
+    while (true)
+    {
+        Mat imgOriginal;
+
+        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+        if (!bSuccess) //if not success, break loop
+        {
+            cout << "Cannot read a frame from video stream" << endl;
+            break;
+        }
+
+        Mat imgHSV;
+
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+        Mat imgThresholded;
+
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+        //morphological opening (remove small objects from the foreground)
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+        dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+        //morphological closing (fill small holes in the foreground)
+        dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+        imshow("Thresholded Image", imgThresholded); //show the thresholded image
+        imshow("Original", imgOriginal); //show the original image
+
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+        {
+            cout << "esc key is pressed by user" << endl;
+            break;
+        }
+    }
+
+    return 0;
 
 }
-

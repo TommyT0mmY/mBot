e7b9dce83a1fa5c0b45eea59f87886810333f18c
emanuele ricci lucchi <emanuele.riccilucchi@gmail.com>
2022-01-07 10:58:04 +0100

    basic Warp

--- mBot/mBot.cpp
+++ mBot/mBot.cpp
@@ -10,73 +10,91 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    VideoCapture cap(0); //capture the video from web cam
+	VideoCapture cap(0); //capture the video from web cam
 
-    if (!cap.isOpened())  // if not success, exit program
-    {
-        cout << "Cannot open the web cam" << endl;
-        return -1;
-    }
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the web cam" << endl;
+		return -1;
+	}
 
-    namedWindow("Control"); //create a window called "Control"
+	namedWindow("Control"); //create a window called "Control"
 
-    int iLowH = 0;
-    int iHighH = 104;
+	int iLowH = 0;
+	int iHighH = 104;
 
-    int iLowS = 61;
-    int iHighS = 191;
+	int iLowS = 61;
+	int iHighS = 191;
 
-    int iLowV = 219;
-    int iHighV = 255;
+	int iLowV = 219;
+	int iHighV = 255;
 
-    //Create trackbars in "Control" window
-    createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-    createTrackbar("HighH", "Control", &iHighH, 179);
+	//Create trackbars in "Control" window
+	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 179);
 
-    createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-    createTrackbar("HighS", "Control", &iHighS, 255);
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
 
-    createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
-    createTrackbar("HighV", "Control", &iHighV, 255);
+	createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
 
-    while (true)
-    {
-        Mat imgOriginal;
 
-        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+	while (true)
+	{
+		Mat imgOriginal;
 
-        if (!bSuccess) //if not success, break loop
-        {
-            cout << "Cannot read a frame from video stream" << endl;
-            break;
-        }
 
-        Mat imgHSV;
+		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
 
-        Mat imgThresholded;
+		float w = 250;
+		float h = 250;
 
-        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+		Mat matrix;
+		Mat imgWarp;
 
-        //morphological opening (remove small objects from the foreground)
-        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-        //dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		//i punti nell'immagine
+		Point2f src[4] = { {100,100},{100,200},{300,50},{300,250} };
+		//i punti nell'immagine di destinazione comprende altezza e larghezza
+		Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
 
-        //morphological closing (fill small holes in the foreground)
-        //dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-        //erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		matrix = getPerspectiveTransform(src, dst);
+		warpPerspective(imgOriginal, imgWarp, matrix, Point(w, h));
 
-        imshow("Thresholded Image", imgThresholded); //show the thresholded image
-        imshow("Original", imgOriginal); //show the original image
+		imshow("image", imgWarp);
 
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-        {
-            cout << "esc key is pressed by user" << endl;
-            break;
-        }
-    }
+		Mat imgHSV;
 
-    return 0;
+		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+		Mat imgThresholded;
+
+		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+		//morphological opening (remove small objects from the foreground)
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		//dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//morphological closing (fill small holes in the foreground)
+		//dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		//erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		imshow("Thresholded Image", imgThresholded); //show the thresholded image
+		imshow("Original", imgOriginal); //show the original image
+
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
+
+	return 0;
 
 }

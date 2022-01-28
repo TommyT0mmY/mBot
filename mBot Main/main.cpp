6895fdd71a64a0a27c8cedc205a5114eef01cf06
Tommaso Goni <44438981+TommyT0mmY@users.noreply.github.com>
2022-01-28 10:38:59 +0100

    Merge main

---
+++ mBot Main/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <chrono>
+#include "..\mBot Backend\mBot Backend.h"
+#include "..\mBot Temp Frontend\mBot Temp Frontend.h"
+
+int main() {
+
+	std::cout << "hello world\n";
+
+	Backend backend;
+
+	Frontend frontend;
+
+	auto start = std::chrono::high_resolution_clock::now();
+	auto end = std::chrono::high_resolution_clock::now();
+
+	std::chrono::nanoseconds Counter(0);
+	std::chrono::nanoseconds NanosecondsPerFrame((int)(1.f / (float)60 * 1e9));
+
+	while (1) {
+		start = std::chrono::high_resolution_clock::now();
+
+		if (Counter >= NanosecondsPerFrame) {
+			Counter = std::chrono::nanoseconds(0);
+
+			DataTransfer data = backend.update();
+			frontend.update(data);
+		}
+
+		end = std::chrono::high_resolution_clock::now();
+
+		Counter += end - start;
+	}
+
+	std::cout << "end\n";
+}

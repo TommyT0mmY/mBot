146dc51bfb08b1291781aff39a5d11b08658f538
Giuseppe Casadio <g.casadiospam@gmail.com>
2022-02-02 09:48:38 +0100

    Inseriti nel progetto le opencv 4.5.4 prese da  https://sourceforge.net/projects/opencvlibrary/files/4.5.4/opencv-4.5.4-vc14_vc15.exe/download per non riempire tutto ho cancellato quello che non dovrebbe servire. L'elenco dei file cancellati è in doc\openCVDeleted.txt

--- mBot Backend/libs/include/Field.cpp
+++ mBot Backend/libs/include/Field.cpp
@@ -16,6 +16,10 @@ void Field::normalizeField() {
 
 void Field::update() {
 	Field::normalizeField();
+	
+	//riga da eliminare inserita solo per poter compialre
+	cv::Mat fieldImage=cv::Mat(100, 100, CV_32FC1,0);
+
 
 	for (auto detector : detectors) {
 		detector->update(fieldImage);

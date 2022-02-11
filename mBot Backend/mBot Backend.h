03bc7cc70f1ccc79107d88a032f86c48794d81b2
Tommaso Goni <tommasogo99@gmail.com>
2022-02-11 10:42:45 +0100

    Aggiornati include progetto

--- mBot Backend/mBot Backend.h
+++ mBot Backend/mBot Backend.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include "libs/include/BallDetector.h"
-#include "libs/include/PlayerDetector.h"
-#include "libs/include/Field.h"
-
 struct DataTransfer {
 	//data
 	//PointsData field;

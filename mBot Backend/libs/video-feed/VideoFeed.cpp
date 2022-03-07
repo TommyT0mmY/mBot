b1e9eca480a34600ac205bd0884d63b600b4acee
Tommaso Goni <44438981+TommyT0mmY@users.noreply.github.com>
2022-03-07 10:49:04 +0100

    Update VideoFeed.cpp

--- mBot Backend/libs/video-feed/VideoFeed.cpp
+++ mBot Backend/libs/video-feed/VideoFeed.cpp
@@ -1,9 +1,30 @@
 #include "VideoFeed.h"
 
 void VideoFeed::update() {
-
+    videoCapture.grab();
+    bool result = videoCapture.retrieve(*image);
+    if (!result) image = nullptr;
 }
 
 cv::Mat* VideoFeed::getImage() {
     return image;
+}
+
+cv::Mat* VideoFeed::nextImage() {
+    update();
+    return getImage();
+}
+
+VideoFeed::VideoFeed(std::string filename) {
+    this->filename = filename;
+    update(); // sets to first image
+}
+
+VideoFeed::VideoFeed(int cameraId) {
+    this->cameraId = cameraId;
+    update(); // sets to first image
+}
+
+VideoFeed::~VideoFeed() {
+
 }

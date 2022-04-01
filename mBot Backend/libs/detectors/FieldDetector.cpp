6b6180eedf8879b6827ecc22a17f4aa6cd2c8cc0
Tommaso Goni <44438981+TommyT0mmY@users.noreply.github.com>
2022-04-01 10:47:32 +0200

    Iniziata l'implementazione del FieldDetector
    Per ora il codice scritto è per il debug del detector e non funziona correttamente

--- mBot Backend/libs/detectors/FieldDetector.cpp
+++ mBot Backend/libs/detectors/FieldDetector.cpp
@@ -1,12 +1,49 @@
 #pragma once
 
 #include "FieldDetector.h"
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <opencv2/highgui.hpp>
 
 void FieldDetector::update(cv::Mat& image) {
+    bool detectResult = qrCodeDetector.detectAndDecodeMulti(image, updateDecodeInfo, updateCorners);
+    CV_UNUSED(detectResult);
 
+    cv::Mat copy = image.clone();
+    cv::Scalar color(255, 0, 0);
+    cv::circle(copy, cv::Point2f(10.f, 10.f), 100, color, cv::FILLED);
+
+    cv::pollKey();
+    
+    for (cv::Point currPoint : updateCorners) {
+        cv::circle(copy, currPoint, 20, color, cv::FILLED);
+    }
+
+    for (int i = 0; i < updateDecodeInfo.size(); ++i) {
+        std::string currCode = updateDecodeInfo[i];
+        /*
+        if (currCode == "TOP_LEFT")
+            
+        if (currCode == "TOP_RIGHT")
+
+        if (currCode == "BOTTOM_LEFT")
+
+        if (currCode == "BOTTOM_RIGHT")
+*/
+    }
+
+    cv::imshow("DEBUG FIELD DETECTOR", copy);
 }
 
 
 Field FieldDetector::getField() {
     return field;
+}
+
+FieldDetector::FieldDetector(){
+
+}
+
+FieldDetector::~FieldDetector() {
+
 }

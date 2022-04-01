b527f211ac2bf4a408efb79d58dbc516713a0eab
Tommaso Goni <44438981+TommyT0mmY@users.noreply.github.com>
2022-04-01 10:48:28 +0200

    Iniziata l'implementazione del FieldDetector
    Per ora il codice scritto è per il debug del detector e non funziona correttamente

--- mBot Backend/libs/detectors/FieldDetector.h
+++ mBot Backend/libs/detectors/FieldDetector.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/objdetect.hpp>
+#include <vector>
+#include <string>
 #include "Detector.h"
 #include "../objects/Field.h"
 
@@ -8,9 +11,17 @@
 class FieldDetector : public IDetector {
 private:
 	Field field;
+	cv::QRCodeDetector qrCodeDetector;
+
+	//Vettori utilizzati da update per detectare i QR Code
+	std::vector<std::string> updateDecodeInfo;
+	std::vector<cv::Point> updateCorners;
 
 public:
 	void update(cv::Mat& image);
 
 	Field getField();
+
+	FieldDetector();
+	~FieldDetector();
 };

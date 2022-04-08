8bca8263b26deeec74e607a6c121d6b01a15ddcd
emanuele ricci lucchi <emanuele.riccilucchi@gmail.com>
2022-04-08 10:39:20 +0200

    incluso il modulo di opencv per utilizzare i fiducials aruco e implementata la base del field detector i file sono stati modificati leggermente per riuscire a complilare visto che i path di inclusione sono diversi

--- mBot Backend/libs/detectors/FieldDetector.cpp
+++ mBot Backend/libs/detectors/FieldDetector.cpp
@@ -1,36 +1,23 @@
 #pragma once
 
 #include "FieldDetector.h"
+#include "../external/aruco/include/opencv2/aruco.hpp"
 #include <opencv2/imgproc.hpp>
-#include <iostream>
 #include <opencv2/highgui.hpp>
+#include <iostream>
 
 void FieldDetector::update(cv::Mat& image) {
-    bool detectResult = qrCodeDetector.detectAndDecodeMulti(image, updateDecodeInfo, updateCorners);
-    CV_UNUSED(detectResult);
-
     cv::Mat copy = image.clone();
-    cv::Scalar color(255, 0, 0);
-    cv::circle(copy, cv::Point2f(10.f, 10.f), 100, color, cv::FILLED);
+
+    std::vector<int> markerIds;
+    std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+    cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_5X5_50);
+    cv::aruco::detectMarkers(copy, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
+
+    cv::aruco::drawDetectedMarkers(copy, markerCorners, markerIds);
 
     cv::pollKey();
-    
-    for (cv::Point currPoint : updateCorners) {
-        cv::circle(copy, currPoint, 20, color, cv::FILLED);
-    }
-
-    for (int i = 0; i < updateDecodeInfo.size(); ++i) {
-        std::string currCode = updateDecodeInfo[i];
-        /*
-        if (currCode == "TOP_LEFT")
-            
-        if (currCode == "TOP_RIGHT")
-
-        if (currCode == "BOTTOM_LEFT")
-
-        if (currCode == "BOTTOM_RIGHT")
-*/
-    }
 
     cv::imshow("DEBUG FIELD DETECTOR", copy);
 }
--- mBot Backend/libs/detectors/FieldDetector.h
+++ mBot Backend/libs/detectors/FieldDetector.h
@@ -1,17 +1,15 @@
 #pragma once
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/objdetect.hpp>
-#include <vector>
-#include <string>
 #include "Detector.h"
 #include "../objects/Field.h"
+#include <opencv2/core/core.hpp>
+#include <vector>
+#include <string>
 
 
 class FieldDetector : public IDetector {
 private:
 	Field field;
-	cv::QRCodeDetector qrCodeDetector;
 
 	//Vettori utilizzati da update per detectare i QR Code
 	std::vector<std::string> updateDecodeInfo;
